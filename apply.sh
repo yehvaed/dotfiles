a9f6b3044f09a75515aa32c97099dc72186022c7
yehvaed <jaroslaw.walach@gmail.com>
2024-02-07 13:31:48 +0100

    style(dotfiles): improve apply script

--- apply.sh
+++ apply.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+echo ">>> 📦 Install dependecies..."
 ansible-galaxy install --role-file=requirements.yaml
+echo 
+
+echo ">>> 📦 Apply configuration..."
 ansible-playbook main.yaml
+
+echo ">>> 🚀 Shell reloading..."
+exec ${SHELL}
